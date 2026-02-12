d0fab72518b290208fcc0d1135578b9e4dd2f656
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2026-02-12 16:23:23 +0530

    Creating a connection - first instance use of Magic numbers like: 0, 1, 2, 3. Next using another way instead of Magic Numbers

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 }); // Create a WebSocket server on port 8080;
+
+//Connection Event
+// Socket: Represents the individual connection between the server and a client.
+// Request: Contains information about the client's request, such as headers and URL.
+
+wss.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress; // Get the client's IP Address.
+
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+
+    console.log({ rawData: rawData, message: message });
+
+    wss.clients.forEach((client) => {
+      // DIFFERENT STATES
+      // 0: CONNECTING - The connection is not yet open.
+      // 1: OPEN - The connection is open and ready to communicate.
+      // 2: CLOSING - The connection is in the process of closing.
+      // 3: CLOSED - The connection is closed or couldn't be opened.
+      //   if (client.readyState === 1) {
+      //     client.send(`Server Broadcast: ${message} from ${ip}`);
+      //   }
+
+      // Instead of using the numeric value 1, we can use the WebSocket.OPEN constant for better readability and maintainability.
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(`Server Broadcast: ${message} from ${ip}`);
+      }
+    });
+  });
+});
